6ac1ad5d1b7d18412253ebe751efdedcc5b485a3
ArmanGrewal007 <armansinghgrewal@gmail.com>
2024-10-04 00:41:46 +0530

    added automation

--- _automate.sh
+++ _automate.sh
@@ -1,3 +1,16 @@
 #! /usr/bin/env bash
 
+# Define the architecture types
+ARCHITECTURES=("amd64" "arm64")
 
+for ARCH in "${ARCHITECTURES[@]}"; do
+    # Scan packages and generate the Packages file
+    dpkg-scanpackages --arch "$ARCH" pool/main/ > "dists/stable/main/a/armangrewal007/binary-$ARCH/Packages"
+    # Compress the Packages file
+    gzip -9 < "dists/stable/main/a/armangrewal007/binary-$ARCH/Packages" > "dists/stable/main/a/armangrewal007/binary-$ARCH/Packages.gz"
+
+    echo "Generated Package file for $ARCH architecture."
+done
+
+apt-ftparchive release dists/stable > dists/stable/Release
+echo "Updated release too"
