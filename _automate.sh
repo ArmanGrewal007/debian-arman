cd55eb85ce84c1fd135024e0489f4fb61abfe831
Arman Grewal <armansinghrewal@gmail.com>
2024-10-06 15:22:53 +0000

    updated

--- _automate.sh
+++ _automate.sh
@@ -5,9 +5,9 @@ ARCHITECTURES=("amd64" "arm64")
 
 for ARCH in "${ARCHITECTURES[@]}"; do
     # Scan packages and generate the Packages file
-    dpkg-scanpackages --arch "$ARCH" pool/main/ > "dists/stable/main/a/armangrewal007/binary-$ARCH/Packages"
+    dpkg-scanpackages --arch "$ARCH" pool/main/ > "dists/stable/main/binary-$ARCH/Packages"
     # Compress the Packages file
-    gzip -9 < "dists/stable/main/a/armangrewal007/binary-$ARCH/Packages" > "dists/stable/main/a/armangrewal007/binary-$ARCH/Packages.gz"
+    gzip -9 < "dists/stable/main/binary-$ARCH/Packages" > "dists/stable/main/binary-$ARCH/Packages.gz"
 
     echo "Generated Package file for $ARCH architecture."
 done
