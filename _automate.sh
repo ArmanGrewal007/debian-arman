73dace2f93a0d75b3547391a1a9fe2a033b8cbe5
Arman Singh Grewal <66531687+ArmanGrewal007@users.noreply.github.com>
2024-10-06 22:52:39 +0530

    Update _automate.sh

--- _automate.sh
+++ _automate.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env bash
 
+# Author: Arman Grewal
+#################################################
+# Automate the process of creating Packages file
+# Creating tgz of it, and a Release file
+# ... run this after you change deb packages
+#################################################
+
 # Define the architecture types
 ARCHITECTURES=("amd64" "arm64")
 
@@ -14,3 +21,25 @@ done
 
 apt-ftparchive release dists/stable > dists/stable/Release
 echo "Updated release too"
+
+############# Push to github #############
+git config --global user.name "Arman Grewal"
+git config --global user.email "armansinghrewal@gmail.com"
+
+YELLOW_IT="\e[33;3m"
+YELLOW_BOLD="\e[33;1m"
+RESET="\e[0m"
+echo -e "${YELLOW_BOLD}TIP:${RESET} ${YELLOW_IT}can abort this step using ctrl+z, then you can access the terminal${RESET}"
+echo -e "${YELLOW_IT}you can run git diff, or anything you deem necessary${RESET}"
+echo -e "${YELLOW_IT}further lines are just add, commit and push to git${RESET}"
+
+read -p "Are you sure you want to add and commit? (y/n): " confirmation
+if [[ "$confirmation" != "y" ]]; then
+    echo "Operation canceled."
+    exit 0
+fi
+
+# Commit to git
+git add .
+git commit -m "Updated release"
+git push
